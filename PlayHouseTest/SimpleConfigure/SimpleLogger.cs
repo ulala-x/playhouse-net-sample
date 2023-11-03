79a00633dd9c627a9b2e2a1470c0c3366fd7fd00
blackstorm-ulalax <ulalax@blackstormcompany.com>
2023-11-03 14:23:54 +0900

    서버 안정성 테스트

---
+++ PlayHouseTest/SimpleConfigure/SimpleLogger.cs
@@ -0,0 +1,38 @@
+﻿using PlayHouse.Utils;
+using Serilog;
+
+namespace SimpleConfigure
+{
+    public class SimpleLogger : IPlayHouseLogger
+    {
+        public void Debug(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Debug($"[{className}] {messageFactory()}");
+        }
+
+        public void Error(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Error($"[{className}] {messageFactory()}");
+        }
+
+        public void Fatal(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Fatal($"[{className}] {messageFactory()}");
+        }
+
+        public void Info(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Information($"[{className}] {messageFactory()}");
+        }
+
+        public void Trace(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Verbose($"[{className}] {messageFactory()}");
+        }
+
+        public void Warn(Func<string> messageFactory, string className)
+        {
+            Log.Logger.Warning($"[{className}] {messageFactory()}");
+        }
+    }
+}
