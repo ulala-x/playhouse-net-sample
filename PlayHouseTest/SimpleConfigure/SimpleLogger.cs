0d7aa3454051293b2f4533d7f48378b31cd94abc
blackstorm-ulalax <ulalax@blackstormcompany.com>
2023-11-07 14:24:27 +0900

    [ulalax] - logging with mehtod name

--- PlayHouseTest/SimpleConfigure/SimpleLogger.cs
+++ PlayHouseTest/SimpleConfigure/SimpleLogger.cs
@@ -5,34 +5,34 @@ namespace SimpleConfigure
 {
     public class SimpleLogger : IPlayHouseLogger
     {
-        public void Debug(Func<string> messageFactory, string className)
+        public void Debug(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Debug($"[{className}] {messageFactory()}");
+            Log.Logger.Debug($"[{className}] ({methodName}) {messageFactory()}");
         }
 
-        public void Error(Func<string> messageFactory, string className)
+        public void Error(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Error($"[{className}] {messageFactory()}");
+            Log.Logger.Error($"[{className}] ({methodName}) {messageFactory()}");
         }
 
-        public void Fatal(Func<string> messageFactory, string className)
+        public void Fatal(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Fatal($"[{className}] {messageFactory()}");
+            Log.Logger.Fatal($"[{className}] ({methodName}) {messageFactory()}");
         }
 
-        public void Info(Func<string> messageFactory, string className)
+        public void Info(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Information($"[{className}] {messageFactory()}");
+            Log.Logger.Information($"[{className}] ({methodName}) {messageFactory()}");
         }
 
-        public void Trace(Func<string> messageFactory, string className)
+        public void Trace(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Verbose($"[{className}] {messageFactory()}");
+            Log.Logger.Verbose($"[{className}] ({methodName}) {messageFactory()}");
         }
 
-        public void Warn(Func<string> messageFactory, string className)
+        public void Warn(Func<string> messageFactory, string className,string methodName)
         {
-            Log.Logger.Warning($"[{className}] {messageFactory()}");
+            Log.Logger.Warning($"[{className}] ({methodName}) {messageFactory()}");
         }
     }
 }
